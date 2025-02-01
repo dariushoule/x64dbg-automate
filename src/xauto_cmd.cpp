ab817d4df167445074f5cf050e2462476c880c8c
Darius Houle <dariushoule@gmail.com>
2025-01-31 21:03:20 -0700

    rw registers

--- src/xauto_cmd.cpp
+++ src/xauto_cmd.cpp
@@ -138,6 +138,140 @@ void dbg_write_memory(msgpack::object root, msgpack::sbuffer& response_buffer) {
 
 void dbg_read_regs(msgpack::sbuffer& response_buffer) {
     REGDUMP rd;
-    DbgGetRegDump(&rd); // WIP
-    msgpack::pack(response_buffer, rd);
+    DbgGetRegDumpEx(&rd, sizeof(rd));
+
+    std::array<uint8_t, 80> reg_area;
+    std::copy_n((uint8_t*)&rd.regcontext.RegisterArea[0], 80, reg_area.begin()); 
+    std::array<uint8_t, 16 * 16> xmm_regs;
+    std::copy_n((uint8_t*)&rd.regcontext.XmmRegisters[0], 16 * 16, xmm_regs.begin()); 
+    std::array<uint8_t, 16 * 32> ymm_regs;
+    std::copy_n((uint8_t*)&rd.regcontext.YmmRegisters[0], 16 * 32, ymm_regs.begin()); 
+
+    FpuRegsArr fpu_regs;
+    for (size_t i = 0; i < 8; i++) {
+        fpu_regs[i] = FpuRegsTup(
+            std::array<uint8_t, 10>(),
+            rd.x87FPURegisters[i].st_value,
+            rd.x87FPURegisters[i].tag
+        );
+        std::copy_n((uint8_t*)&rd.x87FPURegisters[i].data[0], 10, std::get<0>(fpu_regs[i]).begin()); 
+    }
+
+    std::array<uint8_t, 128> lastError;
+    std::copy_n((uint8_t*)&rd.lastError.name[0], 128, lastError.begin());
+    std::array<uint8_t, 128> lastStatus;
+    std::copy_n((uint8_t*)&rd.lastStatus.name[0], 128, lastStatus.begin());
+
+    #ifdef _WIN64
+    std::tuple<
+        size_t, 
+        CtxTup64, 
+        FlagsTup, 
+        FpuRegsArr, 
+        MmxArr,
+        MxcsrFieldsTup,
+        x87StatusWordFieldsTup,
+        x87ControlWordFieldsTup,
+        std::tuple<uint32_t, std::array<uint8_t, 128>>,
+        std::tuple<uint32_t, std::array<uint8_t, 128>>
+    > regdump(
+        64,
+        CtxTup64(
+            rd.regcontext.cax, rd.regcontext.cbx, rd.regcontext.ccx, rd.regcontext.cdx, rd.regcontext.cbp, rd.regcontext.csp, rd.regcontext.csi, rd.regcontext.cdi,
+            rd.regcontext.r8, rd.regcontext.r9, rd.regcontext.r10, rd.regcontext.r11, rd.regcontext.r12, rd.regcontext.r13, rd.regcontext.r14, rd.regcontext.r15,
+            rd.regcontext.cip,
+            rd.regcontext.eflags,
+            rd.regcontext.cs, rd.regcontext.ds, rd.regcontext.es, rd.regcontext.fs, rd.regcontext.gs, rd.regcontext.ss,
+            rd.regcontext.dr0, rd.regcontext.dr1, rd.regcontext.dr2, rd.regcontext.dr3, rd.regcontext.dr6, rd.regcontext.dr7,
+            reg_area,
+            x87fpuTup(
+                rd.regcontext.x87fpu.ControlWord,
+                rd.regcontext.x87fpu.StatusWord,
+                rd.regcontext.x87fpu.TagWord,
+                rd.regcontext.x87fpu.ErrorOffset,
+                rd.regcontext.x87fpu.ErrorSelector,
+                rd.regcontext.x87fpu.DataOffset,
+                rd.regcontext.x87fpu.DataSelector,
+                rd.regcontext.x87fpu.Cr0NpxState
+            ),
+            rd.regcontext.MxCsr,
+            xmm_regs,
+            ymm_regs
+        ),
+        FlagsTup(rd.flags.c, rd.flags.p, rd.flags.a, rd.flags.z, rd.flags.s, rd.flags.t, rd.flags.i, rd.flags.d, rd.flags.o),
+        fpu_regs,
+        MmxArr {rd.mmx[0], rd.mmx[1], rd.mmx[2], rd.mmx[3], rd.mmx[4], rd.mmx[5], rd.mmx[6], rd.mmx[7]},
+        MxcsrFieldsTup(
+            rd.MxCsrFields.FZ, rd.MxCsrFields.PM, rd.MxCsrFields.UM, rd.MxCsrFields.OM,
+            rd.MxCsrFields.ZM, rd.MxCsrFields.IM, rd.MxCsrFields.DM, rd.MxCsrFields.DAZ,
+            rd.MxCsrFields.PE, rd.MxCsrFields.UE, rd.MxCsrFields.OE, rd.MxCsrFields.ZE,
+            rd.MxCsrFields.DE, rd.MxCsrFields.IE, rd.MxCsrFields.RC
+        ),
+        x87StatusWordFieldsTup(
+            rd.x87StatusWordFields.B, rd.x87StatusWordFields.C3, rd.x87StatusWordFields.C2, rd.x87StatusWordFields.C1, rd.x87StatusWordFields.C0,
+            rd.x87StatusWordFields.ES, rd.x87StatusWordFields.SF, rd.x87StatusWordFields.P, rd.x87StatusWordFields.U, rd.x87StatusWordFields.O, 
+            rd.x87StatusWordFields.Z, rd.x87StatusWordFields.D, rd.x87StatusWordFields.I, rd.x87StatusWordFields.TOP),
+        x87ControlWordFieldsTup(
+            rd.x87ControlWordFields.IC, rd.x87ControlWordFields.IEM, rd.x87ControlWordFields.PM, rd.x87ControlWordFields.UM, rd.x87ControlWordFields.OM, 
+            rd.x87ControlWordFields.ZM, rd.x87ControlWordFields.DM, rd.x87ControlWordFields.IM, rd.x87ControlWordFields.RC, rd.x87ControlWordFields.PC),
+        std::tuple<uint32_t, std::array<uint8_t, 128>>(rd.lastError.code, lastError),
+        std::tuple<uint32_t, std::array<uint8_t, 128>>(rd.lastStatus.code, lastStatus)
+    );
+    #else
+    std::tuple<
+        size_t, 
+        CtxTup64, 
+        FlagsTup, 
+        FpuRegsArr, 
+        MmxArr,
+        MxcsrFieldsTup,
+        x87StatusWordFieldsTup,
+        x87ControlWordFieldsTup,
+        std::tuple<uint32_t, std::array<uint8_t, 128>>,
+        std::tuple<uint32_t, std::array<uint8_t, 128>>
+    > regdump(
+        32,
+        CtxTup32(
+            rd.regcontext.cax, rd.regcontext.cbx, rd.regcontext.ccx, rd.regcontext.cdx, rd.regcontext.cbp, rd.regcontext.csp, rd.regcontext.csi, rd.regcontext.cdi,
+            rd.regcontext.cip,
+            rd.regcontext.eflags,
+            rd.regcontext.cs, rd.regcontext.ds, rd.regcontext.es, rd.regcontext.fs, rd.regcontext.gs, rd.regcontext.ss,
+            rd.regcontext.dr0, rd.regcontext.dr1, rd.regcontext.dr2, rd.regcontext.dr3, rd.regcontext.dr6, rd.regcontext.dr7,
+            reg_area,
+            x87fpuTup(
+                rd.regcontext.x87fpu.ControlWord,
+                rd.regcontext.x87fpu.StatusWord,
+                rd.regcontext.x87fpu.TagWord,
+                rd.regcontext.x87fpu.ErrorOffset,
+                rd.regcontext.x87fpu.ErrorSelector,
+                rd.regcontext.x87fpu.DataOffset,
+                rd.regcontext.x87fpu.DataSelector,
+                rd.regcontext.x87fpu.Cr0NpxState
+            ),
+            rd.regcontext.MxCsr,
+            xmm_regs,
+            ymm_regs
+        ),
+        FlagsTup(rd.flags.c, rd.flags.p, rd.flags.a, rd.flags.z, rd.flags.s, rd.flags.t, rd.flags.i, rd.flags.d, rd.flags.o),
+        fpu_regs,
+        MmxArr {rd.mmx[0], rd.mmx[1], rd.mmx[2], rd.mmx[3], rd.mmx[4], rd.mmx[5], rd.mmx[6], rd.mmx[7]},
+        MxcsrFieldsTup(
+            rd.MxCsrFields.FZ, rd.MxCsrFields.PM, rd.MxCsrFields.UM, rd.MxCsrFields.OM,
+            rd.MxCsrFields.ZM, rd.MxCsrFields.IM, rd.MxCsrFields.DM, rd.MxCsrFields.DAZ,
+            rd.MxCsrFields.PE, rd.MxCsrFields.UE, rd.MxCsrFields.OE, rd.MxCsrFields.ZE,
+            rd.MxCsrFields.DE, rd.MxCsrFields.IE, rd.MxCsrFields.RC
+        ),
+        x87StatusWordFieldsTup(
+            rd.x87StatusWordFields.B, rd.x87StatusWordFields.C3, rd.x87StatusWordFields.C2, rd.x87StatusWordFields.C1, rd.x87StatusWordFields.C0,
+            rd.x87StatusWordFields.ES, rd.x87StatusWordFields.SF, rd.x87StatusWordFields.P, rd.x87StatusWordFields.U, rd.x87StatusWordFields.O, 
+            rd.x87StatusWordFields.Z, rd.x87StatusWordFields.D, rd.x87StatusWordFields.I, rd.x87StatusWordFields.TOP),
+        x87ControlWordFieldsTup(
+            rd.x87ControlWordFields.IC, rd.x87ControlWordFields.IEM, rd.x87ControlWordFields.PM, rd.x87ControlWordFields.UM, rd.x87ControlWordFields.OM, 
+            rd.x87ControlWordFields.ZM, rd.x87ControlWordFields.DM, rd.x87ControlWordFields.IM, rd.x87ControlWordFields.RC, rd.x87ControlWordFields.PC),
+        std::tuple<uint32_t, std::array<uint8_t, 128>>(rd.lastError.code, lastError),
+        std::tuple<uint32_t, std::array<uint8_t, 128>>(rd.lastStatus.code, lastStatus)
+    );
+    #endif
+
+    msgpack::pack(response_buffer, regdump);
 }
--- src/xauto_cmd.h
+++ src/xauto_cmd.h
@@ -29,4 +29,36 @@ typedef std::tuple<size_t, size_t, uint32_t, uint16_t, size_t, uint32_t, uint32_
 void dbg_memmap(msgpack::sbuffer& response_buffer);
 void dbg_read_memory(msgpack::object root, msgpack::sbuffer& response_buffer);
 void dbg_write_memory(msgpack::object root, msgpack::sbuffer& response_buffer);
+
+
+typedef std::tuple<uint16_t, uint16_t, uint16_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t> x87fpuTup;
+typedef std::tuple<
+    size_t, size_t, size_t, size_t, size_t, size_t, size_t, size_t, size_t, size_t, size_t, size_t, size_t, size_t, size_t, size_t, // gp regs
+    size_t, size_t, // ip, flags
+    uint16_t, uint16_t, uint16_t, uint16_t, uint16_t, uint16_t, // segs
+    size_t, size_t, size_t, size_t, size_t, size_t, // dregs
+    std::array<uint8_t, 80>,
+    x87fpuTup,
+    uint32_t,
+    std::array<uint8_t, 16 * 16>,
+    std::array<uint8_t, 16 * 32>
+> CtxTup64;
+typedef std::tuple<
+    size_t, size_t, size_t, size_t, size_t, size_t, size_t, size_t, // gp regs
+    size_t, size_t, // ip, flags
+    uint16_t, uint16_t, uint16_t, uint16_t, uint16_t, uint16_t, // segs
+    size_t, size_t, size_t, size_t, size_t, size_t, // dregs
+    std::array<uint8_t, 80>,
+    x87fpuTup,
+    uint32_t,
+    std::array<uint8_t, 8 * 16>,
+    std::array<uint8_t, 8 * 32>
+> CtxTup32;
+typedef std::tuple<bool, bool, bool, bool, bool, bool, bool, bool, bool> FlagsTup;
+typedef std::tuple<std::array<uint8_t, 10>, size_t, size_t> FpuRegsTup;
+typedef std::array<FpuRegsTup, 8> FpuRegsArr;
+typedef std::array<uint64_t, 8> MmxArr;
+typedef std::tuple<bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, uint16_t> MxcsrFieldsTup;
+typedef std::tuple<bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, uint16_t> x87StatusWordFieldsTup;
+typedef std::tuple<bool, bool, bool, bool, bool, bool, bool, bool, uint16_t, uint16_t> x87ControlWordFieldsTup;
 void dbg_read_regs(msgpack::sbuffer& response_buffer);
